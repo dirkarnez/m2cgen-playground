e03029bcd5784fe376d8c0dac6786c10ea89fabd
dirkarnez <smalldirkalex@gmail.com>
2024-09-20 21:35:23 +0800

    - wip

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from sklearn.datasets import load_diabetes
+from sklearn import linear_model
+import m2cgen as m2c
+
+X, y = load_diabetes(return_X_y=True)
+
+estimator = linear_model.LinearRegression()
+estimator.fit(X, y)
+
+code = m2c.export_to_c(estimator)
